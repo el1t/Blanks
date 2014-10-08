9f5d0c274f5c7203d515c736474ceadcf44a2575
Ellis <el1t@ymail.com>
2014-10-07 23:23:16 -0400

    Initial Commit
    Fetching questions and UI works!

---
+++ app/src/main/java/com/el1t/bah/GameActivity.java
@@ -0,0 +1,151 @@
+package com.el1t.bah;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.inputmethod.InputMethodManager;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+
+public class GameActivity extends Activity implements GamePickFragment.OnFragmentInteractionListener{
+	private static final String TAG = "GAME";
+	private URL SERVER;
+	private GamePickFragment mPickFragment;
+	private InputMethodManager mInputMethodManager;
+	private String answer;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_game);
+		mPickFragment = new GamePickFragment();
+
+		if (savedInstanceState == null) {
+			getFragmentManager().beginTransaction()
+					.add(R.id.container, mPickFragment)
+					.commit();
+		}
+
+		try {
+			SERVER = new URL("http://blanks.herokuapp.com");
+		} catch (MalformedURLException e) {
+			Log.e(TAG, "URL problem!", e);
+		}
+
+		mInputMethodManager = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+
+		requestCard();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.regular, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+	@Override
+	public void onPause() {
+		super.onPause();
+
+		mInputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+	}
+
+	// Handles answer submission
+	public void submit(String answer) {
+		this.answer = answer;
+		mInputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+		mPickFragment.clear();
+		requestCard();
+	}
+
+	// Updates the card text with a new question
+	protected void updateCard(String response) {
+		mPickFragment.update(response.replace("\"", ""), "First Version");
+	}
+
+	// Queries server for a random card
+	protected void requestCard() {
+		new WebConnection().execute(SERVER);
+	}
+
+	// AsyncTask to handle contacting the server
+	private class WebConnection extends AsyncTask<URL, Void, String> {
+		private static final String TAG = "CONNECTION";
+
+		@Override
+		protected String doInBackground(URL... urls) {
+			assert(urls.length == 1);
+			URL url = urls[0];
+			HttpURLConnection urlConnection;
+			String response = "";
+			try {
+				urlConnection = (HttpURLConnection) url.openConnection();
+				InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+				response = readStream(in);
+				urlConnection.disconnect();
+			} catch (Exception e) {
+				Log.e(TAG, "Connection error.", e);
+			}
+			return response;
+		}
+
+		@Override
+		protected void onPostExecute(String result) {
+			super.onPostExecute(result);
+			//Do anything with response..
+			updateCard(result);
+		}
+
+		private String readStream(InputStream is) {
+		/*
+		 * To convert the InputStream to String we use the BufferedReader.readLine()
+		 * method. We iterate until the BufferedReader return null which means
+		 * there's no more data to read. Each line will appended to a StringBuilder
+		 * and returned as String.
+		 */
+			BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+			StringBuilder sb = new StringBuilder();
+
+			String line;
+			try {
+				while ((line = reader.readLine()) != null) {
+					sb.append(line + "\n");
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			} finally {
+				try {
+					is.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return sb.toString();
+		}
+	}
+}
---
+++ app/src/main/java/com/el1t/bah/GamePickFragment.java
@@ -0,0 +1,72 @@
+package com.el1t.bah;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+/**
+ * Created by El1t on 10/7/14.
+ */
+public class GamePickFragment extends Fragment {
+	private OnFragmentInteractionListener mListener;
+	private TextView cardTitle;
+	private TextView pack;
+	private EditText answer;
+	private Button submit;
+
+	public interface OnFragmentInteractionListener {
+		public void submit(String answer);
+		public void finish();
+	}
+
+	public GamePickFragment() {
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		View rootView = inflater.inflate(R.layout.fragment_game_picker, container, false);
+
+		cardTitle   = (TextView)rootView.findViewById(R.id.card_title);
+		pack        = (TextView)rootView.findViewById(R.id.pack);
+		answer      = (EditText)rootView.findViewById(R.id.answer);
+		submit      = (Button)  rootView.findViewById(R.id.submit);
+
+		submit.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				mListener.submit(answer.getText().toString());
+			}
+		});
+
+		return rootView;
+	}
+
+	@Override
+	public void onAttach(Activity activity) {
+		super.onAttach(activity);
+
+		// This makes sure that the container activity has implemented
+		// the callback interface. If not, it throws an exception
+		try {
+			mListener = (OnFragmentInteractionListener) activity;
+		} catch (ClassCastException e) {
+			throw new ClassCastException(activity.toString()
+					+ " must implement OnFragmentInteractionListener");
+		}
+	}
+
+	public void update(String title, String pack) {
+		cardTitle.setText(title);
+		this.pack.setText(pack);
+	}
+
+	public void clear() {
+		answer.setText("");
+	}
+}
